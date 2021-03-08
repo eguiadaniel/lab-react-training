2e03f3e80043a1f4d1f11c62f04f474a299188ea
fnwebdev <publicidad@fantasynails.es>
2021-03-08 14:51:42 +0100

    iteration 14 done

--- src/FaceBook.jsx
+++ src/FaceBook.jsx
@@ -2,53 +2,57 @@ import React, { Component } from 'react';
 import profiles from './data/berlin.json';
 import './FaceBook.css';
 
+class Facebook extends React.Component {
+  state = {
+    profiles: profiles,
+    selectedCountry : null
+  }
 
-class Facebook extends Component {
+  handleCountrySelect = (country) => {
+    this.setState({
+      selectedCountry: country
+    })
+    console.log(country)
+  }
+
   render() {
-    // const {
-    //   name,
-    //   rating,
-    //   img
-    // } = this.props;
-
-    console.log(profiles)
-
-    // const listItems = profiles.forEach((profile) => {
-    // console.log(profile.firstname))}
     
-  const listItems = profiles.map((profile) =>
-  // <div className="">{profile.firstName}</div>
-  
-  <div key={profile.firstname + profile.lastname} className="FaceBook">
-  {/* <div className="FaceBook-img-container"> */}
-  {/* <div className="FaceBook-img"> */}
-          <img className="FaceBook-img" src={profile.img} alt={profile.lastname}></img>
-            <ul> 
-              <li><strong>First Name: </strong>{profile.firstName}</li>
-              <li><strong>Last Name: </strong>{profile.lastName}</li>
-              <li><strong>Country: </strong>{profile.country}</li>
-              <li><strong>Type: </strong>{profile.isStudent && "Student" || "Teacher"}</li>
-            </ul>
-        {/* </div> */}
-        {/* </div> */}
-      </div>)
+    const countries = [];
+    for (const profile of this.state.profiles) 
+      {const country = profile.country;
+      if(!countries.includes(country)){
+        countries.push(country);
+      }
+    }
 
     return (
-      <div className="Facebook">
-         {listItems}
-        {/*<div>
-          <img src={profile.img}></img>
-            <ul> 
-              <li>{profile.firstname}</li>
-              <li>{profile.lastname}</li>
-              <li>{profile.country}</li>
-              <li>{profile.type}</li>
-            </ul>
-        </div> */}
+    <div>
+      <span>{this.state.selectedCountry}</span><br></br>
+      <button onClick={() => this.handleCountrySelect(null)} className={'facebook-btn ' + (this.selectedCountry === null ? 'highlighted' : '')}>All</button>
+      {countries.map(country => <button key={country} onClick={() => this.handleCountrySelect(country)} className={'facebook-btn ' + (this.state.selectedCountry === country ? 'highlighted' : '')}>{country}</button>)}
+
+      {this.state.profiles.map(profile => (
+
+        <div key={profile.img} className={"profile " + (!this.state.selectedCountry || this.state.selectedCountry === profile.country ? 'highlighted' : '')}> 
+          
+          <img className="profile__image" src={profile.img} alt={profile.firstName+profile.lastName} />
+          
+          <ul className="profile__details">
+            <li><strong>First Name: </strong>{profile.firstName}</li>
+            <li><strong>Last Name: </strong>{profile.lastName}</li>
+            <li><strong>Country: </strong>{profile.country}</li>
+            <li><strong>Type: </strong> {profile.isStudent ? 'Student' : 'Teacher'}</li>
+          </ul>
+        
+        </div>
+      
+      ))}
       </div>
-    );
-  }
+  );
 }
+}
+
+
 
 export default Facebook;
 
